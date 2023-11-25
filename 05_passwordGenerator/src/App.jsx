d79b7dbeb0bb7d7f836d63a18cd97c243b19584a
PACode2121 <code@pakistanautomation.com.pk>
2023-11-25 19:10:24 +0500

    5th project password generator completed

---
+++ 05_passwordGenerator/src/App.jsx
@@ -0,0 +1,87 @@
+import { useState, useCallback, useEffect,  useRef } from 'react'
+
+function App() {
+  const [numAllowed, setNumAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [length, setLength] = useState(8)
+  const [password, setPassword] = useState(0)
+
+  const passwordRef = useRef(null)
+  
+  const passwordGenerator = useCallback(() => {
+      let pass = ''
+      let passChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      passChar += passChar.toLowerCase()
+
+      if(numAllowed) passChar += '0123456789'
+      if(charAllowed) passChar += '!@#$%^&*()~'
+
+      for (let i = 0; i < length; i++) {
+        let char = Math.floor(Math.random() * passChar.length + 1)
+        pass += passChar.charAt(char)
+      }
+      setPassword(pass)
+    }, [length, numAllowed, charAllowed, setPassword])
+  
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password])
+  
+  useEffect(()=>(passwordGenerator()), [length, numAllowed, charAllowed])
+
+  return (
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+          <h1 className='text-white text-center my-3'>Password generator</h1>
+          <div className="flex shadow rounded-lg overflow-hidden mb-4">
+            <input
+                type="text"
+                value={password}
+                className="outline-none w-full py-1 px-3"
+                placeholder="Password"
+                readOnly
+                ref={passwordRef}
+            />
+            <button
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+            onClick={copyPassword}
+            >copy</button>  
+          </div>
+          <div className='flex text-sm gap-x-2'>
+            <div className='flex items-center gap-x-1'>
+              <input 
+              type="range"
+              min={8}
+              max={100}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e)=>(setLength(e.target.value))}
+              />
+              <label>Length: {length}</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numAllowed}
+              id="numberInput"
+              onChange={(e)=>(setNumAllowed(!numAllowed))}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+            </div>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                id="characterInput"
+              onChange={(e)=>(setCharAllowed(!charAllowed))}
+              />
+                <label htmlFor="characterInput">Characters</label>
+            </div>
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       >
         <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <MyButton color='red' setColor={setBgColor}/>
+            <MyButton color='red' setCeolor={setBgColor}/>
             <MyButton color='green' setColor={setBgColor}/>
             <MyButton color='blue' setColor={setBgColor}/>
             <MyButton color='olive' setColor={setBgColor}/>
